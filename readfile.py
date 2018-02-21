382cd3143a600edfa9c2ce240a547e7a0dca3add
siwei <sisama2323@gmail.com>
2018-02-20 16:26:11 -0800

    add readfile

--- readfile.py
+++ readfile.py
@@ -2,10 +2,21 @@ import numpy as np
 import os.path as osp
 import os
 import sys
-from util import music
+from util import *
+import torch 
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+
 
 
 input_file = 'CSE_253_P4/data/input.txt'
+# input_file = 'data/input.txt'
 # sample_music = 'data/sample-music.txt'
 
 # music_book contain all the information about every song, including beats, name, ryh
@@ -15,7 +26,9 @@ sheet = []
 with open(input_file, 'r') as f:
     for i, line in enumerate(f):
         # check the first 7 letter
-        if line[0:7] == '<start>':
+        if len(line) <= 3:
+            pass
+        elif line[0:7] == '<start>':
             # initialize a music
             music_book.append(music())
             # initialize body
@@ -26,22 +39,38 @@ with open(input_file, 'r') as f:
             except ValueError:
                 music_book[-1].ref_num = line[2]
         elif line[0] == 'T':
-            music_book[-1].tune = line[2:-4]
+            music_book[-1].tune = line[2:-1]
         elif line[0] == 'M':
-            music_book[-1].beats = line[2:-4]
+            music_book[-1].beats = line[2:-1]
         elif line[0] == 'L':
-            music_book[-1].note_len = line[2:-4]
+            music_book[-1].note_len = line[2:-1]
         elif line[0] == 'K':
-            music_book[-1].key = line[2:-4]
+            music_book[-1].key = line[2:-1]
         elif line[0] == 'L':
-            music_book[-1].note_len = line[2:-4]
+            music_book[-1].note_len = line[2:-1]
         # store body
         elif line[1] != ':' and line[0] != '<' and line[0] != '<':
-            body += line[:-4]
+            body += line[:-1]
         elif line[0:5] == '<end>':
             music_book[-1].body = body
             sheet.append(body)
 
 
-a=5
 
+for piece in sheet:
+    lineToTensor(piece)
+
+input = Variable(lineToTensor(piece))
+# train_input = trainset.train_data[train_idx, :, :, :]
+# train_labels = [trainset.train_labels[j] for j in train_idx]
+
+# val_input = trainset.train_data[val_idx, :, :, :]
+# val_labels = [trainset.train_labels[j] for j in val_idx]
+# # prepare for training set and validation set
+# train_set = torch.utils.data.TensorDataset(torch.from_numpy(train_input.reshape(44998, 3, 32, 32)), torch.from_numpy(np.array(train_labels)))
+# val_set = torch.utils.data.TensorDataset(torch.from_numpy(val_input.reshape(5000, 3, 32, 32)), torch.from_numpy(np.array(val_labels)))
+
+# trainloader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True, num_workers=2)
+# validationloader = torch.utils.data.DataLoader(val_set, batch_size=4, shuffle=False, num_workers=2)
+
+a=5
--- util.py
+++ util.py
@@ -1,4 +1,13 @@
+from __future__ import unicode_literals, print_function, division
 import numpy as np 
+import unicodedata
+from io import open
+import glob
+import string
+import torch
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
 
 
 class music():
@@ -12,3 +21,31 @@ class music():
         self.note_len = L
         self.key = K
         self.body = Body
+
+# Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+import torch
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
